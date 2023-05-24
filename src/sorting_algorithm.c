5cd6eb63936c87fe65b4a9d8a516de778d5c789b
itissteo <matteo.buergler@gmail.com>
2023-05-25 01:48:48 +0200

    250523_working_prototype

--- src/sorting_algorithm.c
+++ src/sorting_algorithm.c
@@ -6,7 +6,7 @@
 /*   By: mburgler <mburgler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/15 22:42:24 by mburgler          #+#    #+#             */
-/*   Updated: 2023/05/24 18:00:00 by mburgler         ###   ########.fr       */
+/*   Updated: 2023/05/25 01:48:22 by mburgler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,28 +71,6 @@ int	elements_in_stack(t_list *stack)
 
 void	binary_radix_sort(t_msc *msc)
 {
-// 	int		nb_count;
-// 	int		shift;
-// 	int		controlled_index;
-// 	t_list	*tmp;
-
-// 	shift = 0;
-// 	nb_count = msc->nb_of_indexes;
-// 	tmp = msc->s_a;
-// 	while(nb_count)
-// 	{
-// 		while(nb_of_indexes)
-// 		{
-// 			controlled_index = tmp->index;
-// 			if((tmp->index >> shift) & 1)
-// 				push_b(msc, msc->s_a);
-// 			else
-// 				rotate_a(msc, msc->s_a);
-// 			msc->nb_bits--;
-// 		}
-// 	}
-// // OLD ABOVE
-
 	int	bitshift;
 	int	nb_elements;
 
@@ -111,7 +89,7 @@ void	binary_radix_sort(t_msc *msc)
 		nb_elements = elements_in_stack(msc->s_b);
 		while(nb_elements != 0)
 		{
-			if((msc->s_b->index >> (bitshift + 1)) & 0)
+			if(((msc->s_b->index >> (bitshift + 1)) & 1) == 0)
 				push(msc, msc->s_a, msc->s_b); //push_to; push_from
 			else
 				rotate(msc, msc->s_b);
