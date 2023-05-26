08a14c2308c47d2e408ddb1f6af8749cf478b567
itissteo <matteo.buergler@gmail.com>
2023-05-26 19:29:49 +0200

    new

--- src/sorting_algorithm.c
+++ src/sorting_algorithm.c
@@ -6,7 +6,7 @@
 /*   By: mburgler <mburgler@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/15 22:42:24 by mburgler          #+#    #+#             */
-/*   Updated: 2023/05/26 16:53:03 by mburgler         ###   ########.fr       */
+/*   Updated: 2023/05/26 19:29:13 by mburgler         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,8 +63,8 @@ void	binary_radix_sort(t_msc *msc)
 		nb_elements = elements_in_stack(msc->s_a);
 		while (nb_elements != 0)
 		{
-			if (check_if_sorted(msc) == 1)
-				push_back(msc);
+			// if (check_if_sorted(msc) == 1)
+			// 	push_back(msc);
 			if (((msc->s_a->index >> bitshift) & 1) == 0)
 				push(msc, msc->s_b, msc->s_a);
 			else
@@ -82,8 +82,8 @@ void	binary_radix_sort_p2(t_msc *msc, int nb_elements, int bitshift)
 {
 	while (nb_elements != 0)
 	{
-		if (check_if_sorted(msc) == 1)
-			push_back(msc);
+		// if (check_if_sorted(msc) == 1)
+		// 	push_back(msc);
 		if (((msc->s_b->index >> (bitshift + 1)) & 1) == 1)
 			push(msc, msc->s_a, msc->s_b);
 		else
